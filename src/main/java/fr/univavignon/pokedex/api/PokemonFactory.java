c3cc77b5928dc29043a2b81451ad06324b2f1baa
Hamza Latrach <hamza.latrach098@gmail.com>
2024-11-13 12:38:09 +0100

    Ajout de class PockemonFactory et tests

---
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -0,0 +1,20 @@
+package fr.univavignon.pokedex.api;
+
+/**
+ * Concrete implementation of the IPokemonFactory interface.
+ *
+ * @author fv
+ */
+public class PokemonFactory implements IPokemonFactory {
+
+    @Override
+    public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+        int attackIv = (int) (Math.random() * 15);
+        int defenseIv = (int) (Math.random() * 15);
+        int staminaIv = (int) (Math.random() * 15);
+
+        double iv = (attackIv + defenseIv + staminaIv) / 45.0;
+
+        return new Pokemon(index, "PokemonName", attackIv, defenseIv, staminaIv, cp, hp, dust, candy, iv);
+    }
+}
