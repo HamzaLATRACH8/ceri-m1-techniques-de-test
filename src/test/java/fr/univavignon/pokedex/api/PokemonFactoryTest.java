64d53d8ae0649efbae22219d79a07886ffe8a672
Hamza Latrach <hamza.latrach098@gmail.com>
2024-12-19 00:05:10 +0100

    ajout de test pour RocketPokemonFactory

--- src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
@@ -6,35 +6,46 @@ import static org.junit.Assert.*;
 
 public class PokemonFactoryTest {
 
-    private IPokemonFactory pokemonFactory;
+    private RocketPokemonFactory pokemonFactory;
 
     @Before
     public void setUp() {
-        pokemonFactory = new PokemonFactory();
+        pokemonFactory = new RocketPokemonFactory();
     }
+
 
     @Test
     public void testCreatePokemon() {
-        int index = 1;
-        int cp = 500;
-        int hp = 150;
-        int dust = 1000;
-        int candy = 100;
+        Pokemon pokemon = pokemonFactory.createPokemon(1, 500, 60, 3000, 3);
+
+        assertEquals(1, pokemon.getIndex());
+        assertEquals("Bulbasaur", pokemon.getName());
+        assertEquals(500, pokemon.getCp());
+        assertEquals(60, pokemon.getHp());
+        assertEquals(3000, pokemon.getDust());
+        assertEquals(3, pokemon.getCandy());
+    }
+
+    @Test
+    public void testCreatePokemonWithInvalidIndex() {
+        int index = -1;
+        int cp = 300;
+        int hp = 100;
+        int dust = 500;
+        int candy = 50;
 
         Pokemon pokemon = pokemonFactory.createPokemon(index, cp, hp, dust, candy);
 
         assertEquals(index, pokemon.getIndex());
-        assertEquals("PokemonName", pokemon.getName());
+        assertEquals("Ash's Pikachu", pokemon.getName()); // Correspond au mapping pour l'index -1
         assertEquals(cp, pokemon.getCp());
         assertEquals(hp, pokemon.getHp());
         assertEquals(dust, pokemon.getDust());
         assertEquals(candy, pokemon.getCandy());
 
-        double iv = pokemon.getIv();
-        assertTrue(iv >= 0.0 && iv <= 1.0);
-
-        assertTrue(pokemon.getAttack() >= 0 && pokemon.getAttack() <= 15);
-        assertTrue(pokemon.getDefense() >= 0 && pokemon.getDefense() <= 15);
-        assertTrue(pokemon.getStamina() >= 0 && pokemon.getStamina() <= 15);
+        assertEquals(1000, pokemon.getAttack());
+        assertEquals(1000, pokemon.getDefense());
+        assertEquals(1000, pokemon.getStamina());
+        assertEquals(0.0, pokemon.getIv(), 0.01);
     }
 }
